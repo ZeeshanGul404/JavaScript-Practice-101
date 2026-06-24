e88b547788bfd408520a5069d88144b259a791b0
ZeeshanGul404 <zeeshandiscordking@gmail.com>
2026-06-24 05:36:38 -0700

    pushed

---
+++ Programs/Program 1/app.js
@@ -0,0 +1,9 @@
+const number = parseInt(prompt("Enter a number:   "));
+
+if (number > 0) {
+  alert("The Number Is Positive");
+} else if (number === 0) {
+  alert("The Number Is Zero");
+} else {
+  alert("The Number Is Negative");
+}
